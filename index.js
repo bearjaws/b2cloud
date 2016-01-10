330c25953dfb86786995304af9729a25675788be
Michael Barajas <michael.a.barajas@gmail.com>
2016-01-10 10:36:38 -0500

    remove debug code

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var api = require('./api/index.js');
 
-api.upload.uploadFile('./index.js','debugtestlol').then(function(result) {
-  console.log(result);
-}).catch(function(err) {
-  console.log(err);
-})
-
 module.exports = {
   authorizeAccount: api.authorize.getBasicAuth,
   createBucket: api.bucket.createBucket,
